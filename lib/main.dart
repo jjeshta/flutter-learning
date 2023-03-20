8bea42207eacf85ef9f645552753d936773a708a
Jeshta Bhoyedhur <jeshta.bhoyedhur@publicis.mu>
2023-03-20 15:06:10 +0400

    extracting widgets - way 1

--- lib/main.dart
+++ lib/main.dart
@@ -21,34 +21,7 @@ class _QuoteListState extends State<QuoteList> {
     Quote(author: "Erza Scarlet, Fairy Tail", text: "Those painful memories are what help us make it to tomorrow and become stronger."),
   ];
 
-  Widget quoteTemplate(quote) {
-    return Card(
-      margin: const EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0.0),
-      child: Padding(
-        padding: const EdgeInsets.all(12.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget> [
-            Text(
-            quote.text,
-            style: TextStyle(
-                color: Colors.grey[600],
-                fontSize: 18.0
-              ),
-            ),
-            const SizedBox(height: 6.0,),
-            Text(
-              quote.author,
-              style: TextStyle(
-                  color: Colors.grey[600],
-                  fontSize: 13.0
-              ),
-            ),
-          ],
-        ),
-      )
-    );
-  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -60,9 +33,45 @@ class _QuoteListState extends State<QuoteList> {
       ),
       body: Column(
         children: quotes.map(
-                (quote) => quoteTemplate(quote)
+                (quote) =>  QuoteCard(quote: quote,)
         ).toList(),
       ),
+    );
+  }
+}
+
+class QuoteCard extends StatelessWidget {
+  //final make the data to be passed in this statelesswidget
+  final Quote quote;
+  const QuoteCard({super.key, required this.quote});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+        margin: const EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0.0),
+        child: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget> [
+              Text(
+                quote.text,
+                style: TextStyle(
+                    color: Colors.grey[600],
+                    fontSize: 18.0
+                ),
+              ),
+              const SizedBox(height: 6.0,),
+              Text(
+                quote.author,
+                style: TextStyle(
+                    color: Colors.grey[600],
+                    fontSize: 13.0
+                ),
+              ),
+            ],
+          ),
+        )
     );
   }
 }
--- lib/quote.dart
+++ lib/quote.dart
@@ -1,7 +1,7 @@
 
 class Quote {
-  String? text;
-  String? author;
+  String text;
+  String author;
 
-  Quote({required String this.text, required String this.author});
+  Quote({required this.text, required this.author});
 }
